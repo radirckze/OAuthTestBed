3ee29d3d287a26a4683f9f10f3741c64a44f2a83
Ravi Dirckze <r_dirckze@yahoo.com>
2020-03-10 19:25:18 -0700

    backup checkin 03112020

---
+++ GitLabOAuthClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using OAuthClientBase;
+
+namespace GitLabOAuthClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Type any text and <enter> to terminate:");
+            string inputStr = string.Empty;
+            do
+            {
+                inputStr = Utilities.MaskedRead();
+                Console.WriteLine(String.Format("The input string is {0}", inputStr));
+            }
+            while (inputStr != null && inputStr.Length > 1);
+
+
+        }
+    }
+}
---
+++ OAuthClientBase/Utilities.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OAuthClientBase
+{
+    public class Utilities
+    {
+
+        public static string MaskedRead()
+        {
+            StringBuilder inputStr = new StringBuilder();
+            while (true)
+            {
+                ConsoleKeyInfo i = Console.ReadKey(true);
+                if (i.Key == ConsoleKey.Enter)
+                {
+                    break;
+                }
+                else if (i.Key == ConsoleKey.Backspace)
+                {
+                    if (inputStr.Length > 0)
+                    {
+                        inputStr.Remove(inputStr.Length-1, 1);
+                        Console.Write("\b \b");
+                    }
+                }
+                else 
+                {
+                    inputStr.Append(i.KeyChar); //not checking for invalid characters
+                    Console.Write("*");
+                }
+            }
+            return inputStr.ToString(); ;
+        }
+    }
+}
